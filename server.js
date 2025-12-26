f2022702ba792cf711cee0a6cbe1f894bfd8bf6e
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2025-12-26 09:09:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const { GoogleGenAI } = require("@google/genai");
 
 // --- DATABASE & MODELS ---
-const connectDB = require("./database/db.js");
+const mongoose = require("mongoose");
 const userSchema = require("./database/models/user.schema.js");
 const agreementSchema = require("./database/models/agreement.schema.js");
 
@@ -25,8 +25,18 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 const publicPath = path.join(__dirname, './public');
 
 // --- INITIALIZE DB ---
-connectDB();
+const mongoURI = process.env.MONGODB_URI;
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('✅MongoDB connected successfully!');
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1); 
+  }
+};
+connectDB()
 // --- GLOBAL MIDDLEWARE ---
 app.use((req, res, next) => {
     // Security headers for SharedArrayBuffer (Zama requirement)
