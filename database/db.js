15048352e619017730ea3906ba77c8272196f6e8
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2025-12-26 07:07:07 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/Contracks';
+const mongoURI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
