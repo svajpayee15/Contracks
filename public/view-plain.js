a3397c412e3eb5c7ac6bd0f1f822404ddd4db543
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2025-12-26 11:02:12 +0530

    Update view-plain.js

--- public/view-plain.js
+++ public/view-plain.js
@@ -1,5 +1,5 @@
 import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
-import { deriveKeyFromSeed, decryptFile } from "./crypto.js";
+import { deriveKeyFromSeed, decryptFile } from "https://contracks.vercel.app/crypto.js";
 
 // ⚠️ REPLACE WITH YOUR DEPLOYED PLAIN AGREEMENT ADDRESS
 const CONTRACT_ADDRESS = "0x02f397462F05CCEf87F3ED51793B4b7bB9524A49"; 
