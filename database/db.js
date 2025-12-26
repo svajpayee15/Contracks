79bec42bf4680c40cbfe9e70f1e735d05a6b7685
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2025-12-26 09:12:28 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,16 +1,16 @@
-require("dotenv").config()
-const mongoose = require('mongoose');
+// require("dotenv").config()
+// const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI;
+// const mongoURI = process.env.MONGODB_URI;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI);
-    console.log('✅MongoDB connected successfully!');
-  } catch (err) {
-    console.error(`MongoDB connection error: ${err.message}`);
-    process.exit(1); 
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI);
+//     console.log('✅MongoDB connected successfully!');
+//   } catch (err) {
+//     console.error(`MongoDB connection error: ${err.message}`);
+//     process.exit(1); 
+//   }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
