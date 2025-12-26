7511b0f35c15520e5ef774ebebd6ec9ee3fc4b86
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2025-12-26 09:03:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+require('dotenv').config();
 const express = require("express");
 const path = require('path');
 const cors = require("cors");
