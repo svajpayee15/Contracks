4be4c8de55bb646cbfd9b0a18254905ba0e96138
Shubham Vajpayee <prathamvajpayee65@gmail.com>
2026-02-24 03:21:21 +0530

    Uncomment contract deployment and logging statements

--- config/deploy.js
+++ config/deploy.js
@@ -7,25 +7,25 @@ async function main() {
   console.log("Using account:", deployer.address);
   console.log("----------------------------------------------------");
 
-  // const Salary = await hre.ethers.deployContract("SalaryTemplate");
-  // const Perf = await hre.ethers.deployContract("PerformanceTemplate");
-  // const FFP = await hre.ethers.deployContract("FFPTemplate");
+  const Salary = await hre.ethers.deployContract("SalaryTemplate");
+  const Perf = await hre.ethers.deployContract("PerformanceTemplate");
+  const FFP = await hre.ethers.deployContract("FFPTemplate");
   const Vote = await hre.ethers.deployContract("PlainAgreementTemplate");
 
-  // await Salary.waitForDeployment();
-  // await Perf.waitForDeployment();
-  // await FFP.waitForDeployment();
+  await Salary.waitForDeployment();
+  await Perf.waitForDeployment();
+  await FFP.waitForDeployment();
   await Vote.waitForDeployment();
 
-  // const salaryAddress = await Salary.getAddress();
-  // const perfAddress = await Perf.getAddress();
-  // const ffpAddress = await FFP.getAddress();
+  const salaryAddress = await Salary.getAddress();
+  const perfAddress = await Perf.getAddress();
+  const ffpAddress = await FFP.getAddress();
   const voteAddress = await Vote.getAddress();
 
-  // console.log(`✅ SalaryTemplate CA: ${salaryAddress}`);
-  // console.log(`✅ PerformanceTemplate CA: ${perfAddress}`);
-  // console.log(`✅ FFPTemplate CA: ${ffpAddress}`);
-  // console.log(`✅ BoardVotingTemplate CA: ${voteAddress}`);
+  console.log(`✅ SalaryTemplate CA: ${salaryAddress}`);
+  console.log(`✅ PerformanceTemplate CA: ${perfAddress}`);
+  console.log(`✅ FFPTemplate CA: ${ffpAddress}`);
+  console.log(`✅ BoardVotingTemplate CA: ${voteAddress}`);
   console.log(`✅ PlainAgreementTemplate CA: ${voteAddress}`);
   console.log("----------------------------------------------------");
 }
